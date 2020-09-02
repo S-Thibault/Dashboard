f099408e5f0a393aefa827c6826a692fe38229e5
S-Thibault <sylvain.thibault@infamail.com>
2020-09-02 14:44:02 +0200

    front

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 include 'view/header.php';
 ?>
+
 <!-- Vertical navbar -->
 <div class="vertical-nav bg-white" id="sidebar">
   <div class="py-4 px-3 mb-4 bg-light">
-    <div class="media d-flex align-items-center">
+    <div class="media d-flex align-items-center"><img src="https://i.ibb.co/FbX5PCM/toppng-com-dashboard-svg-icon-free-dashboard-icon-981x823.png" alt="..." width="65" class="mr-3 rounded-circle img-thumbnail shadow-sm">
       <div class="media-body">
-
+        <h4 class="m-0">Dashboard</h4>
       </div>
     </div>
   </div>
@@ -16,27 +17,27 @@ include 'view/header.php';
   <ul class="nav flex-column bg-white mb-0">
     <li class="nav-item">
       <a href="#" class="nav-link text-dark font-italic bg-light">
-        <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
-        Home
-      </a>
+                <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+                Home
+            </a>
     </li>
     <li class="nav-item">
       <a href="#" class="nav-link text-dark font-italic">
-        <i class="fa fa-address-card mr-3 text-primary fa-fw"></i>
-        About
-      </a>
+                <i class="fa fa-address-card mr-3 text-primary fa-fw"></i>
+                About
+            </a>
     </li>
     <li class="nav-item">
       <a href="#" class="nav-link text-dark font-italic">
-        <i class="fa fa-cubes mr-3 text-primary fa-fw"></i>
-        Services
-      </a>
+                <i class="fa fa-cubes mr-3 text-primary fa-fw"></i>
+                Services
+            </a>
     </li>
     <li class="nav-item">
       <a href="#" class="nav-link text-dark font-italic">
-        <i class="fa fa-picture-o mr-3 text-primary fa-fw"></i>
-        Gallery
-      </a>
+                <i class="fa fa-picture-o mr-3 text-primary fa-fw"></i>
+                Gallery
+            </a>
     </li>
   </ul>
 
@@ -45,42 +46,111 @@ include 'view/header.php';
   <ul class="nav flex-column bg-white mb-0">
     <li class="nav-item">
       <a href="#" class="nav-link text-dark font-italic">
-        <i class="fa fa-area-chart mr-3 text-primary fa-fw"></i>
-        Area charts
-      </a>
+                <i class="fa fa-area-chart mr-3 text-primary fa-fw"></i>
+                Area charts
+            </a>
     </li>
     <li class="nav-item">
       <a href="#" class="nav-link text-dark font-italic">
-        <i class="fa fa-bar-chart mr-3 text-primary fa-fw"></i>
-        Bar charts
-      </a>
+                <i class="fa fa-bar-chart mr-3 text-primary fa-fw"></i>
+                Bar charts
+            </a>
     </li>
     <li class="nav-item">
       <a href="#" class="nav-link text-dark font-italic">
-        <i class="fa fa-pie-chart mr-3 text-primary fa-fw"></i>
-        Pie charts
-      </a>
+                <i class="fa fa-pie-chart mr-3 text-primary fa-fw"></i>
+                Pie charts
+            </a>
     </li>
     <li class="nav-item">
       <a href="#" class="nav-link text-dark font-italic">
-        <i class="fa fa-line-chart mr-3 text-primary fa-fw"></i>
-        Line charts
-      </a>
+                <i class="fa fa-line-chart mr-3 text-primary fa-fw"></i>
+                Line charts
+            </a>
     </li>
   </ul>
 </div>
 <!-- End vertical navbar -->
 
-
 <!-- Page content holder -->
 <div class="page-content p-5" id="content">
   <!-- Toggle button -->
-  <button id="sidebarCollapse" type="button" class="btn btn-light bg-white rounded-pill shadow-sm px-4 mb-4"><i class="fa fa-bars mr-2"></i><small class="text-uppercase font-weight-bold">Toggle</small></button>
+  <button id="sidebarCollapse" type="button" class="btn btn-light bg-white rounded-pill shadow-sm px-4 mb-4"><i class="fa fa-bars mr-2"></i><small class="text-uppercase font-weight-bold"></small></button>
+
+  <!-- Demo content -->
+  <h2 class="display-4 text-white"> Gestion d'achat de matériel</h2>
+
+  <!-- Search form -->
+  <div class="flexbox">
+    <div class="search">
+
+      <div>
+        <input type="text" placeholder="Search . . ." required>
+      </div>
+    </div>
+  </div>
+
+  <div class="separator"></div>
 
 
-
-
-
-
+  <div class="container">
+    <div class="row">
+        <div class="col-sm-4 py-2">
+            <div class="card card-body h-100">
+                Card. I'm just a simple card-block.
+            </div>
+        </div>
+        <div class="col-sm-4 py-2">
+            <div class="card h-100 text-white bg-danger">
+                <div class="card-body">
+                    <h3 class="card-title">Danger</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-light">Outline</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 py-2">
+            <div class="card h-100 card-body">
+                Card. I'm just a simple card-block, but I have a little more text!
+            </div>
+        </div>
+        <div class="col-sm-4 py-2">
+            <div class="card h-100 border-primary">
+                <div class="card-body">
+                    <h3 class="card-title">Primary</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-secondary">Outline</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-4 py-2">
+            <div class="card h-100 card-body">
+                Card. I'm just a simple card-block.
+            </div>
+        </div>
+        <div class="col-sm-4 py-2">
+            <div class="card text-white bg-primary">
+                <div class="card-body">
+                    <h3 class="card-title">Hello</h3>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-outline-light">Outline</a>
+                </div>
+            </div>
+        </div>
+      <div class="bg-white p-5 rounded my-5 shadow-sm">
+        <p class="lead font-italic mb-0 text-muted">"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+      </div>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+        dolor.
+      </p>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+        dolor.
+      </p>
+    </div>
+  </div>
+</div>
 
 <?php include 'view/footer.php'; ?>
