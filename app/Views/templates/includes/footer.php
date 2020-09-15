43173447e668a1a5c2e1a2b11e721f3096483334
Emma Leger <e.leger@codeur.online>
2020-09-15 09:45:31 +0200

    css

--- app/Views/templates/includes/footer.php
+++ app/Views/templates/includes/footer.php
@@ -11,57 +11,8 @@
                  */
                 echo $content;
                 ?>
+                <hr>
 
-                <br>
-
-                <div class="row">
-                  <div class="col-sm-8">
-                    <hr>
-                    <canvas id="myChart" height="100"></canvas>
-                    <script>
-                    var ctx = document.getElementById('myChart').getContext('2d');
-                    var myChart = new Chart(ctx, {
-                      type: 'bar',
-                      data: {
-                        labels: ['bricolage', 'éléctroménager', 'multimedia', 'tv-hifi', 'voiture'],
-                        datasets: [{
-                          label: 'Prix moyen des produits en € par catégorie',
-                          data: [49, 120, 60, 89, 66],
-                          backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                          ],
-                          borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                          ],
-                          borderWidth: 1
-                        }]
-                      },
-                      options: {
-                        scales: {
-                          yAxes: [{
-                            ticks: {
-                              beginAtZero: true
-                            }
-                          }]
-                        }
-                      }
-                    });
-                    </script>
-                  </div>
-                  <div class="col-sm-4">
-                  </div>
-
-                </div>
 
             </div>
         </div><!-- /.container -->
@@ -78,5 +29,7 @@
             </div>
           </div>
         </div>
+        <script src="../public/js/script.js"></script>
     </body>
+    
 </html>
--- app/Views/users/login.php
+++ app/Views/users/login.php
@@ -7,14 +7,38 @@
 
 <!-- Formulaire de connexion -->
 <div style="padding-left: 15px;">
-<h2>Login : </h2>
-<h4>Veuillez vous connecter pour accéder à l'admin</h4>
+
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Login</h3>
+			</div>
+			<div class="card-body ">
+        <h4 class="text-white text-center">Veuillez vous connecter pour accéder à l'admin</h4>
+        <br>
+				<form method="post">
+					<div class="input-group form-group text-white">
+						<?= $form->input('username', 'pseudo'); ?>
+					</div>
+					<div class="input-group form-group text-white">
+						<?= $form->input('password', 'mot de passe', ['type' => 'password']); ?>
+					</div>
+					<div class="form-group text-center">
+					<button class="btn btn-primary ">Envoyer</button>
+					</div>
+				</form>
+			</div>
+      <div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Don't have an account?<a href="#">Sign Up</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a href="#">Forgot your password?</a>
+				</div>
+			</div>
+			</div>
+		</div>
+	</div>
 </div>
-<hr>
-<div class="col-sm-4 col-md-4">
-<form method="post">
-    <?= $form->input('username', 'pseudo'); ?>
-    <?= $form->input('password', 'mot de passe', ['type' => 'password']); ?>
-    <button class="btn btn-primary">Envoyer</button>
-</form>
-</div>
+<br>
